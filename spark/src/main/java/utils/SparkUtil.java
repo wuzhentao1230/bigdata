fdca5d155ee62d5177d64cc84571c344e1f7c4de
吴振涛 <wuzhentao@ndrcbd.org>
2018-06-15 17:38:09 +0800

    帅涛

---
+++ spark/src/main/java/utils/SparkUtil.java
@@ -0,0 +1,24 @@
+package utils;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.SparkSession;
+
+/**
+ * 作者    吴振涛
+ * 描述
+ * 创建时间 2018年01月15日
+ * 任务时间
+ * 邮件时间
+ */
+public class SparkUtil {
+   public static SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("taotao");
+
+    //这个普遍是用sql曹操作  新特性包含了其他的context （hivecontext sparkcontext等）
+    public static SparkSession sparkSession = SparkSession
+            .builder()
+            .config(sparkConf)
+            .getOrCreate();
+    //这个普遍进行Rdd操作的
+    public static JavaSparkContext context = new JavaSparkContext(sparkSession.sparkContext());
+}
