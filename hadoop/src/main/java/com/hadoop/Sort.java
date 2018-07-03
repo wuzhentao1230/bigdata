6fa8f13cfa837869956145a239af64f437eceae7
吴振涛 <wuzhentao@ndrcbd.org>
2018-07-03 18:19:50 +0800

    帅涛

---
+++ hadoop/src/main/java/com/hadoop/Sort.java
@@ -0,0 +1,28 @@
+﻿package com.hadoop;
+
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+
+public class Sort extends WritableComparator{
+
+	public Sort()
+	{
+		super(keyPair.class,true);
+	}
+	
+	@Override
+	public int compare(WritableComparable a, WritableComparable b) {
+
+		keyPair k1 = (keyPair)a;
+		keyPair k2 = (keyPair)b;
+		
+		//Integer.compare默认就是升序排序
+		int iRet = Integer.compare(k1.getYear(), k2.getYear());
+		if(iRet != 0)
+		{
+			return iRet;
+		}
+		//温度降序排序
+		return Integer.compare(k2.getTemperature(), k1.getTemperature());
+	}
+}
